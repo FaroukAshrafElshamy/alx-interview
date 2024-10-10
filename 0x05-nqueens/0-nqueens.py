7ccb375a8747847129d9312a73981a466d8f0363
FaroukAshrafElshamy <faroukashrafelshamy321@gmail.com>
2024-10-10 20:46:44 +0300

    N-Queens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,44 +1,38 @@
 #!/usr/bin/python3
+import sys
 """N-Queens"""
 
 
-import sys
-
-
-def is_safe(board, row, col, N):
+def IsSafe(board, row, col, N):
     '''Check the Queen if it could be in the board'''
-    # Check if there is a queen in the same column
     for i in range(row):
         if board[i][col] == 1:
             return False
 
-    # Check upper diagonal on left side
     for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
 
-    # Check upper diagonal on right side
     for i, j in zip(range(row, -1, -1), range(col, N)):
         if board[i][j] == 1:
             return False
-
     return True
 
 
-def solve_nqueens_util(board, row, N, solutions):
+def SolveNqueensUtil(board, row, N, solutions):
     '''Use backtracking to solve the problem'''
     if row == N:
         solutions.append([[i, col] for i, col in enumerate(board[row - 1])])
         return
 
     for col in range(N):
-        if is_safe(board, row, col, N):
+        if IsSafe(board, row, col, N):
             board[row][col] = 1
-            solve_nqueens_util(board, row + 1, N, solutions)
+            SolveNqueensUtil(board, row + 1, N, solutions)
             board[row][col] = 0
 
 
-def solve_nqueens(N):
+def SolveNqueens(N):
     '''Get the solution of N-Queen'''
     if not N.isdigit():
         print("N must be a number")
@@ -51,7 +45,7 @@ def solve_nqueens(N):
 
     board = [[0 for _ in range(N)] for _ in range(N)]
     solutions = []
-    solve_nqueens_util(board, 0, N, solutions)
+    SolveNqueens(board, 0, N, solutions)
 
     for sol in solutions:
         print(sol)
@@ -61,4 +55,4 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-    solve_nqueens(sys.argv[1])
+    SolveNqueens(sys.argv[1])
